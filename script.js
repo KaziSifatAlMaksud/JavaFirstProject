97ca80fd74c6e1464da0df3f7100a9c3b1d3c6df
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2021-10-02 01:19:25 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 'use script'
 const number = Math.trunc(Math.random()*20)+1;
 
-
+//document.querySelector('.gess').textContent = number;
 let score = document.querySelector('.score').textContent;
 let hightscore = document.querySelector('.hight').textContent ;
 document.querySelector('.agin').addEventListener('click',function(){
     document.querySelector('.message').textContent =  "Enter Any value...";
     const number =  Math.trunc(Math.random()*20)+1;
-
-
+    document.querySelector('.input').textContent = NaN;
+  
      score = Number('20');
     document.querySelector('.score').textContent = score;
     document.querySelector('body').style.backgroundColor="gray";
-
+    
 
     document.querySelector('.cheack').addEventListener('click',function(){
         const gass = document.querySelector('.input').value;
@@ -28,7 +28,7 @@ document.querySelector('.agin').addEventListener('click',function(){
         }
         else if(gass==number){
          document.querySelector('.message').textContent =  " Conratulations You are Win";
-
+        document.querySelector('.gess').textContent = number;
          document.querySelector('body').style.backgroundColor="green";
          document.querySelector('.gess').style.width = "15%";
          document.querySelector('.score').textContent = score;
@@ -71,7 +71,7 @@ document.querySelector('.cheack').addEventListener('click',function(){
    }
    else if(gass==number){
     document.querySelector('.message').textContent =  " Conratulations You are Win";
-
+    document.querySelector('.gess').textContent = number;
     document.querySelector('body').style.backgroundColor="green";
     document.querySelector('.gess').style.width = "15%";
     document.querySelector('.score').textContent = score;
